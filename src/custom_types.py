c1c8201eb3749874ec61ecd6d630011bff2b2ec6
ChrisScarred <scarred.chris@gmail.com>
2023-05-09 18:09:21 +0200

    defined custom types

---
+++ src/custom_types.py
@@ -0,0 +1,25 @@
+from typing import Tuple, List, Dict, Annotated
+from numpy.typing import NDArray
+from dataclasses import dataclass
+from scipy.io.wavfile import read as read_wav
+
+@dataclass
+class MinVal:
+    min: int
+
+@dataclass
+class Range:
+    min: int
+    max: int
+
+Index = Annotated[int, MinVal(0)]
+Label = Annotated[int, Range(0, 11)]
+SamplingRate = Annotated[int, read_wav]
+Recording = Annotated[NDArray, read_wav]
+
+WavContent = Tuple[SamplingRate, Recording]
+Datapoint = Tuple[Index, WavContent, Label]
+Data = List[Datapoint]
+SplitData = Tuple[Data, Data, Data]
+Ratios = Dict[str, float]
+Lengths = Tuple[int, int, int]
