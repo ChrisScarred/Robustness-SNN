1c928d5c73b801b7e1771fbd8eaa6880abf5aac4
ChrisScarred <scarred.chris@gmail.com>
2023-05-18 22:20:31 +0200

    Data and DataPoint made into Pydantic's BaseModel classes

--- src/utils/custom_types.py
+++ src/utils/custom_types.py
@@ -1,6 +1,8 @@
-from typing import Tuple, List, Dict, Annotated
-from numpy.typing import NDArray
 from dataclasses import dataclass
+from typing import Annotated, Generator, Callable, Dict, List, Optional
+
+from pydantic import BaseModel
+from pydantic_numpy import NDArray
 from scipy.io.wavfile import read as read_wav
 
 
@@ -16,13 +18,41 @@ class Range:
 
 
 Index = Annotated[int, MinVal(0)]
-Label = Annotated[int, Range(0, 11)]
 SamplingRate = Annotated[int, read_wav]
 Recording = Annotated[NDArray, read_wav]
+Type_ = Annotated[str, "train/test/validation"]
+Label = Annotated[int, Range(0, 11)]
+Recordings = List[Recording]
 
-WavContent = Tuple[SamplingRate, Recording]
-Datapoint = Tuple[Index, WavContent, Label]
-Data = List[Datapoint]
-SplitData = Tuple[Data, Data, Data]
-Ratios = Dict[str, float]
-Lengths = Tuple[int, int, int]
+class DataPoint(BaseModel):
+    index: Index
+    sampling_rate: SamplingRate
+    wav: NDArray
+    label: Label
+    wav_norm: Optional[NDArray] = None
+    mfsc: Optional[NDArray] = None
+    type_: Optional[Type_] = None
+
+
+class Data(BaseModel):
+    data: List[DataPoint]
+
+    def __len__(self) -> int:
+        return len(self.data)
+    
+    def __setitem__(self, index: int, dp: DataPoint) -> None:
+        self.data.insert(index, dp)
+
+    def __getitem__(self, index: int) -> DataPoint:
+        return self.data[index]
+    
+    def __delitem__(self, index: int) -> None:
+        del self.data[index]
+    
+    def __iter__(self) -> Generator[DataPoint, None, None]:
+        for x in self.data:
+            yield x
+
+Ratios = Dict[Type_, float]
+Lengths = Dict[Type_, int]
+Config = Annotated[Callable, "src.utils.config"]
