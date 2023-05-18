ce19e25a80882bc7bc83bedc76fcd2a2ea705b45
ChrisScarred <scarred.chris@gmail.com>
2023-05-18 22:20:54 +0200

    Refactored loading.

--- src/utils/load.py
+++ src/utils/load.py
@@ -1,11 +1,15 @@
 import os
 import pickle
+from numpy.typing import NDArray
 
 from scipy.io.wavfile import read as read_wav
 
-from src.utils.custom_types import Data
+from src.utils.custom_types import Data, DataPoint
 from src.utils.config import Config
 from src.utils.parsing import label_from_fname
+
+def _dp_constructor(i: int, sr: int, wav: NDArray, l: int) -> DataPoint:
+    return DataPoint(index=i, sampling_rate=sr, wav=wav, label=l)
 
 
 def load_wavs(config: Config) -> Data:
@@ -16,12 +20,13 @@ def load_wavs(config: Config) -> Data:
                 return pickle.load(f)
 
     dir_path = config.get("data.tidigits.dir_path")
-    wavs = [
-        (i, read_wav(os.path.join(dir_path, x)), label_from_fname(x))
+    
+    wavs = Data(data=[
+        _dp_constructor(i, *read_wav(os.path.join(dir_path, x)), label_from_fname(x))
         for i, x in enumerate(os.listdir(dir_path))
         if os.path.isfile(os.path.join(dir_path, x))
         and os.path.splitext(os.path.join(dir_path, x))[-1] == ".wav"
-    ]
+    ])
 
     if config.get("data.tidigits.pickle"):
         with open(config.get("data.tidigits.pickle_path"), "wb") as f:
